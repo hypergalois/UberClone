8da023c8cec02778dad1b08dfcc56b640b9b8092
hypergalois <jose.delgado2@live.u-tad.com>
2024-09-24 20:26:06 +0200

    Initial Config

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Home() {
+    return (
+        <SafeAreaView>
+            <Text> hellow rodol </Text>
+        </SafeAreaView>
+    );
+}
