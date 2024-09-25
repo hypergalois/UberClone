7026ca1f8a238710cc58c9deda8fab3d80a4f76d
hypergalois <jose.delgado2@live.u-tad.com>
2024-09-25 12:27:55 +0200

    Create routes structure

---
+++ app/(root)/(tabs)/history.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function History() {
+    return (
+        <SafeAreaView>
+            <Text> History </Text>
+        </SafeAreaView>
+    );
+}
