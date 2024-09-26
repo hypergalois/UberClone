8b1b1062560222b1e2ec850d0f787c83cff098c0
hypergalois <jose.delgado2@live.u-tad.com>
2024-09-26 16:33:55 +0200

    Add user API endpoint for creating new users

---
+++ app/(api)/user+api.ts
@@ -0,0 +1,31 @@
+import { neon } from "@neondatabase/serverless";
+
+const databaseUrl = process.env.NEON_DATABASE_URL!;
+
+export async function POST(req: Request) {
+    try {
+        const sql = neon(databaseUrl);
+
+        const { name, email, clerkId } = await req.json();
+
+        if (!name || !email || !clerkId) {
+            return Response.json(
+                { error: "Missing required fields" },
+                { status: 400 }
+            );
+        }
+
+        const result = await sql`
+        INSERT INTO users (name, email, clerk_id) 
+        VALUES (${name} ${email} ${clerkId})
+    `;
+
+        return Response.json({ data: result }, { status: 201 });
+    } catch (error) {
+        console.log(error);
+        return Response.json(
+            { error: "Internal server error" },
+            { status: 500 }
+        );
+    }
+}
