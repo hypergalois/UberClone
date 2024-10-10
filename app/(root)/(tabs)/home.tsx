c25745bde1a904f846bad5e05ecdc0f879d4e1dc
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-10 03:09:48 +0200

    Refactor home.tsx to remove unnecessary code and improve user location handling

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -150,8 +150,8 @@ export default function Page() {
             });
 
             setUserLocation({
-                latitude: location.coords?.latitude!,
-                longitude: location.coords?.longitude!,
+                latitude: location.coords?.latitude,
+                longitude: location.coords?.longitude,
                 address: `${address[0].name}, ${address[0].region}`,
             });
         }
