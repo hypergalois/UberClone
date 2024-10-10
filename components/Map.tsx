2de737e4afd51725841b1c12bc27e808011ec74f
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-10 03:09:58 +0200

    Refactor Map component to use user and destination locations for initial region calculation

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,22 +1,38 @@
+import { useLocationStore } from "@/store";
 import { Text, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import { calculateRegion } from "@/lib/map";
 
 export default function Map() {
-    const region = {
-        latitude: 56.5244,
-        longitude: 33.3792,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-    };
+    const {
+        userLongitude,
+        userLatitude,
+        destinationLongitude,
+        destinationLatitude,
+    } = useLocationStore();
+
+    console.log(
+        userLongitude,
+        userLatitude,
+        destinationLongitude,
+        destinationLatitude
+    );
+
+    const region = calculateRegion({
+        userLongitude,
+        userLatitude,
+        destinationLongitude,
+        destinationLatitude,
+    });
 
     return (
         <MapView
             provider={PROVIDER_GOOGLE}
             className="w-full h-full rounded-2xl"
-            initialRegion={region}
             tintColor="black"
             mapType="mutedStandard"
             showsPointsOfInterest={false}
+            initialRegion={region}
             showsUserLocation={true}
             userInterfaceStyle="light"
         ></MapView>
