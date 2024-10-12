d9daa476c076e09da08a510696949ab0efe0da87
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-12 18:30:42 +0200

    Refactor store/index.ts to add DriverStore and update LocationStore with new functions and state for managing drivers and selected driver

--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,4 @@
-import { LocationStore } from "@/types/type";
+import { DriverStore, LocationStore, MarkerData } from "@/types/type";
 import { create } from "zustand";
 
 export const useLocationStore = create<LocationStore>((set, get) => ({
@@ -35,6 +35,22 @@ export const useLocationStore = create<LocationStore>((set, get) => ({
             destinationLatitude: latitude,
             destinationLongitude: longitude,
             destinationAddress: address,
-            s,
+        })),
+}));
+
+export const useDriverStore = create<DriverStore>((set, get) => ({
+    drivers: [] as MarkerData[],
+    selectedDriver: null,
+    setSelectedDriver: (driverId: number) =>
+        set(() => ({
+            selectedDriver: driverId,
+        })),
+    setDrivers: (drivers: MarkerData[]) =>
+        set(() => ({
+            drivers: drivers,
+        })),
+    clearSelectedDriver: () =>
+        set(() => ({
+            selectedDriver: null,
         })),
 }));
