1a66ddf2fccc3c6940c983827bb349e6b69cc6d5
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-14 00:41:45 +0200

    Refactor _layout.tsx to include react-native-get-random-values and uuidv4 import

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,6 @@
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
+
 import { Stack } from "expo-router";
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 import * as SplashScreen from "expo-splash-screen";
