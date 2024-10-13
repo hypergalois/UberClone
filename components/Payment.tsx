85a631489f85df9234c6dfece87f6a5f1bbd9ca9
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-13 06:56:29 +0200

    Refactor Payment component to remove unused code and commented out sections

--- components/Payment.tsx
+++ components/Payment.tsx
@@ -18,112 +18,7 @@ const Payment = ({
     driverId,
     rideTime,
 }: PaymentProps) => {
-    const { initPaymentSheet, presentPaymentSheet } = useStripe();
-    const {
-        userAddress,
-        userLongitude,
-        userLatitude,
-        destinationLatitude,
-        destinationAddress,
-        destinationLongitude,
-    } = useLocationStore();
-
-    const { userId } = useAuth();
-    const [success, setSuccess] = useState<boolean>(false);
-
-    const openPaymentSheet = async () => {
-        await initializePaymentSheet();
-
-        const { error } = await presentPaymentSheet();
-
-        if (error) {
-            Alert.alert(`Error code: ${error.code}`, error.message);
-        } else {
-            setSuccess(true);
-        }
-    };
-
-    const initializePaymentSheet = async () => {
-        const { error } = await initPaymentSheet({
-            merchantDisplayName: "Example, Inc.",
-            intentConfiguration: {
-                mode: {
-                    amount: parseInt(amount) * 100,
-                    currencyCode: "usd",
-                },
-                confirmHandler: async (
-                    paymentMethod,
-                    shouldSavePaymentMethod,
-                    intentCreationCallback
-                ) => {
-                    const { paymentIntent, customer } = await fetchAPI(
-                        "/(api)/(stripe)/create",
-                        {
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                            },
-                            body: JSON.stringify({
-                                name: fullName || email.split("@")[0],
-                                email: email,
-                                amount: amount,
-                                paymentMethodId: paymentMethod.id,
-                            }),
-                        }
-                    );
-
-                    if (paymentIntent.client_secret) {
-                        const { result } = await fetchAPI(
-                            "/(api)/(stripe)/pay",
-                            {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                                body: JSON.stringify({
-                                    payment_method_id: paymentMethod.id,
-                                    payment_intent_id: paymentIntent.id,
-                                    customer_id: customer,
-                                    client_secret: paymentIntent.client_secret,
-                                }),
-                            }
-                        );
-
-                        if (result.client_secret) {
-                            await fetchAPI("/(api)/ride/create", {
-                                method: "POST",
-                                headers: {
-                                    "Content-Type": "application/json",
-                                },
-                                body: JSON.stringify({
-                                    origin_address: userAddress,
-                                    destination_address: destinationAddress,
-                                    origin_latitude: userLatitude,
-                                    origin_longitude: userLongitude,
-                                    destination_latitude: destinationLatitude,
-                                    destination_longitude: destinationLongitude,
-                                    ride_time: rideTime.toFixed(0),
-                                    fare_price: parseInt(amount) * 100,
-                                    payment_status: "paid",
-                                    driver_id: driverId,
-                                    user_id: userId,
-                                }),
-                            });
-
-                            intentCreationCallback({
-                                clientSecret: result.client_secret,
-                            });
-                        }
-                    }
-                },
-            },
-            returnURL: "myapp://book-ride",
-        });
-
-        if (!error) {
-            // setLoading(true);
-        }
-    };
+    async function openPaymentSheet() {}
 
     return (
         <>
@@ -134,8 +29,8 @@ const Payment = ({
             />
 
             <ReactNativeModal
-                isVisible={success}
-                onBackdropPress={() => setSuccess(false)}
+            // isVisible={success}
+            // onBackdropPress={() => setSuccess(false)}
             >
                 <View className="flex flex-col items-center justify-center bg-white p-7 rounded-2xl">
                     <Image source={images.check} className="w-28 h-28 mt-5" />
@@ -152,7 +47,7 @@ const Payment = ({
                     <CustomButton
                         title="Back Home"
                         onPress={() => {
-                            setSuccess(false);
+                            // setSuccess(false);
                             router.push("/(root)/(tabs)/home");
                         }}
                         className="mt-5"
