c2a1e324abcf7e620c0796fbcef21e0195c08c94
hypergalois <jose.delgado2@live.u-tad.com>
2024-10-13 07:55:59 +0200

    Refactor Payment component to update URL scheme in BookRide screen

--- app/(api)/(stripe)/create+api.ts
+++ app/(api)/(stripe)/create+api.ts
@@ -12,4 +12,39 @@ export async function POST(req: Request) {
             { status: 400 }
         );
     }
+
+    let customer;
+
+    const doesCustomerExist = await stripe.customers.list({ email });
+
+    if (doesCustomerExist.data.length === 0) {
+        customer = await stripe.customers.create({
+            name,
+            email,
+        });
+    } else customer = doesCustomerExist.data[0];
+
+    const ephemeralKey = await stripe.ephemeralKeys.create(
+        { customer: customer.id },
+        { apiVersion: "2020-08-27" }
+    );
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: parseInt(amount) * 100,
+        currency: "usd",
+        customer: customer.id,
+        automatic_payment_methods: {
+            enabled: true,
+            allow_redirects: "never",
+        },
+    });
+
+    return Response.json(
+        {
+            paymentIntent: paymentIntent,
+            ephemeralKey: ephemeralKey,
+            customer: customer.id,
+        },
+        { status: 201 }
+    );
 }
